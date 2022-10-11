478511ade12edbabe66fec9949b42e74a7a5f051
Aithusa <32020141+Aithosa@users.noreply.github.com>
2022-10-11 23:32:28 +0800

    init commit

---
+++ src/main/java/com/example/simpleredislock/SimpleRedisLockApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleredislock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRedisLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRedisLockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simpleredislock/SimpleRedisLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleredislock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRedisLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
